f0efe1788322a58760523a7820bd429ecfa60de2
Mallikarjun Punna <33911547+mallik30@users.noreply.github.com>
2023-06-25 12:56:05 -0500

    test

--- src/main/java/com/arjun/slack/api/ActionBotManager.java
+++ src/main/java/com/arjun/slack/api/ActionBotManager.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.slack.api.bolt.App;
 import com.slack.api.model.event.AppMentionEvent;
+import com.slack.api.model.event.MessageEvent;
 import com.slack.api.model.event.ReactionAddedEvent;
 
 @Service
@@ -29,6 +30,17 @@ public class ActionBotManager {
 
 	public void appMention() {
 		app.event(AppMentionEvent.class, (payload, ctx) -> {
+			return ctx.ack();
+		});
+	}
+
+	public void messageEvent() {
+		app.event(MessageEvent.class, (payload, ctx) -> {
+			MessageEvent event = payload.getEvent();
+			// Listen only thread replies, if ThreadTs is present it is a reply
+			if (event.getThreadTs() != null) {
+				
+			}
 			return ctx.ack();
 		});
 	}
